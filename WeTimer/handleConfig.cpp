58d051c7066ab42870abd7cdf188176883b92e85
Gauthier <fra589@users.noreply.github.com>
2023-01-30 17:04:36 +0100

    Update copyright

--- WeTimer/handleConfig.cpp
+++ WeTimer/handleConfig.cpp
@@ -1,23 +1,23 @@
-/*************************************************************************/
-/*                                                                       */
-/* Copyright (C) 2021 Gauthier Brière  (gauthier.briere "at" gmail.com)  */
-/*                                                                       */
-/* This file: handleConfig.cpp is part of WeTimer                          */
-/*                                                                       */
-/* WeTimer is free software: you can redistribute it and/or modify it    */
-/* under the terms of the GNU General Public License as published by     */
-/* the Free Software Foundation, either version 3 of the License, or     */
-/* (at your option) any later version.                                   */
-/*                                                                       */
-/* WeTimer is distributed in the hope that it will be useful, but        */
-/* WITHOUT ANY WARRANTY; without even the implied warranty of            */
-/* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         */
-/* GNU General Public License for more details.                          */
-/*                                                                       */
-/* You should have received a copy of the GNU General Public License     */
-/* along with this program.  If not, see <http://www.gnu.org/licenses/>. */
-/*                                                                       */
-/*************************************************************************/
+/****************************************************************************/
+/*                                                                          */
+/* Copyright (C) 2021-2023 Gauthier Brière (gauthier.briere "at" gmail.com) */
+/*                                                                          */
+/* This file: handleConfig.cpp is part of WeTimer                           */
+/*                                                                          */
+/* WeTimer is free software: you can redistribute it and/or modify it       */
+/* under the terms of the GNU General Public License as published by        */
+/* the Free Software Foundation, either version 3 of the License, or        */
+/* (at your option) any later version.                                      */
+/*                                                                          */
+/* WeTimer is distributed in the hope that it will be useful, but           */
+/* WITHOUT ANY WARRANTY; without even the implied warranty of               */
+/* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            */
+/* GNU General Public License for more details.                             */
+/*                                                                          */
+/* You should have received a copy of the GNU General Public License        */
+/* along with this program.  If not, see <http://www.gnu.org/licenses/>.    */
+/*                                                                          */
+/****************************************************************************/
 
 #include <Arduino.h>
 #include "WeTimer.h"
--- WeTimer/handleHttp.cpp
+++ WeTimer/handleHttp.cpp
@@ -1,23 +1,23 @@
-/*************************************************************************/
-/*                                                                       */
-/* Copyright (C) 2021 Gauthier Brière  (gauthier.briere "at" gmail.com)  */
-/*                                                                       */
-/* This file: handleHttp.cpp is part of WeTimer                          */
-/*                                                                       */
-/* WeTimer is free software: you can redistribute it and/or modify it    */
-/* under the terms of the GNU General Public License as published by     */
-/* the Free Software Foundation, either version 3 of the License, or     */
-/* (at your option) any later version.                                   */
-/*                                                                       */
-/* WeTimer is distributed in the hope that it will be useful, but        */
-/* WITHOUT ANY WARRANTY; without even the implied warranty of            */
-/* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         */
-/* GNU General Public License for more details.                          */
-/*                                                                       */
-/* You should have received a copy of the GNU General Public License     */
-/* along with this program.  If not, see <http://www.gnu.org/licenses/>. */
-/*                                                                       */
-/*************************************************************************/
+/****************************************************************************/
+/*                                                                          */
+/* Copyright (C) 2021-2023 Gauthier Brière (gauthier.briere "at" gmail.com) */
+/*                                                                          */
+/* This file: handleHttp.cpp is part of WeTimer                             */
+/*                                                                          */
+/* WeTimer is free software: you can redistribute it and/or modify it       */
+/* under the terms of the GNU General Public License as published by        */
+/* the Free Software Foundation, either version 3 of the License, or        */
+/* (at your option) any later version.                                      */
+/*                                                                          */
+/* WeTimer is distributed in the hope that it will be useful, but           */
+/* WITHOUT ANY WARRANTY; without even the implied warranty of               */
+/* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            */
+/* GNU General Public License for more details.                             */
+/*                                                                          */
+/* You should have received a copy of the GNU General Public License        */
+/* along with this program.  If not, see <http://www.gnu.org/licenses/>.    */
+/*                                                                          */
+/****************************************************************************/
 
 #include <Arduino.h>
 #include "WeTimer.h"
@@ -115,7 +115,7 @@ String piedPage() {
   Buff += APP_VERSION_STRING;
   Buff += F("</p>\n");
   Buff += F(
-    "<p style=\"text-align: center; margin: 0;\">Copyright &copy; 2021 - Gauthier Brière</p>\n"
+    "<p style=\"text-align: center; margin: 0;\">Copyright &copy; 2021-2023 - Gauthier Brière</p>\n"
   );
   Buff += F("</body>\n</html>\n");
 
